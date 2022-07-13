6e33f37f2de376a662ba26ce76b0c5ec335e8067
jasmine <jasminevirk10@gmail.com>
2022-07-13 13:08:51 -0400

    changes

--- server.js
+++ server.js
@@ -9,3 +9,25 @@ const hbs = exphbs.create({ helpers });
 
 const app = express();
 const PORT = process.env.PORT || 3002;
+
+const sequelize = require('./config/connection');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+const sess = {
+    secret: 'Super secret secret',
+    cookie: { expires: 10 * 60 * 1000 },
+    resave: false,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+        db: sequelize
+    })
+};
+app.use(session(sess));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+app.use(routes);
+sequelize.sync({ force: false }).then(() => {
+    app.listen(PORT, () => console.log('Now listening'));
+});
